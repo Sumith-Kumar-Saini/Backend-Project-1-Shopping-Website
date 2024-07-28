e26175fdeeba1b4cab0df5674fe56f685386acbf
Sumith Kumar Saini <sumithsaini01@gmail.com>
2024-07-28 18:30:27 +0530

    Setup Base Routers

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const { log, clear, error } = require('console');
+const { log, clear, error } = require("console");
 
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 const app = express();
+
+const db = require("./config/mongooseConnection");
+
+const productsRouter = require("./routes/productsRouter");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
 
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
-});
+app.get("/owners", ownersRouter);
+app.get("/users", usersRouter);
+app.get("/products", productsRouter);
 
-app.listen(port, function(){
-    clear(); // clearing the console
-    log(`Server is listening on port ${port}`); // Server is listening on port 3000
+app.listen(port, function () {
+  clear(); // clearing the console
+  log(`Server is listening on port ${port}`); // Server is listening on port 3000
 });
--- models/user-model.js
+++ models/user-model.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/scatch_shopping_website");
-
 const userSchema = mongoose.Schema({
   fullname: {
     type: String,
     required: true,
     minlength: 3,
     trim: true,
-    // maxlength: 50,
-    // unique: true,
   },
   email: String,
   password: String,
